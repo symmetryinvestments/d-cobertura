6085fa9b42656f19ed842f3d9f0740c71c82e40b
Elijah Stone <estone@symmetryinvestments.com>
2020-12-15 14:36:29 -0800

    -

--- src/app.d
+++ src/app.d
@@ -33,17 +33,17 @@ struct CoveredFile {
 			.array;
 	}
 
-	string xml_dump() {
+	string xml_dump(string prefix) {
 		import core.stdc.time: time;
 		import std.format: fmt = format;
-		import std.path: dir_name = dirName, base_name = baseName, build_path = buildPath, absolute = absolutePath;
+		import std.path: dir_name = dirName, base_name = baseName, build_path = buildPath, build_path = buildPath, absolute = absolutePath, is_absolute = isAbsolute;
 		import std.algorithm.iteration: splitter;
 		double line_rate = indices.length/cast(double)lines.length;
 		string res = `<?xml version="1.0"?>
 <coverage version="5.3" timestamp="%s" lines-valid="%s" lines-covered="%s" line-rate="%s" branches-covered="0" branches-valid="0" branch-rate="0" complexity="0">`.fmt(time(null), lines.length, indices.length, line_rate);
 		string fpath = build_path(path[0 .. $-4].splitter('-')) ~ ".d";
 		res ~= `<sources><source>%s</source></sources>`.fmt(path.dir_name.absolute);
-		res ~= `<packages><package name="covered" line-rate="%1$s" branch-rate="0" complexity="0"><classes><class name="%2$s" filename="%2$s" complexity="0" line-rate="%1$s" branch-rate="0"><methods/>`.fmt(line_rate, fpath);
+		res ~= `<packages><package name="covered" line-rate="%1$s" branch-rate="0" complexity="0"><classes><class name="%2$s" filename="%2$s" complexity="0" line-rate="%1$s" branch-rate="0"><methods/>`.fmt(line_rate, build_path(prefix, fpath));
 
 		res ~= `<lines>`;
 		foreach (i; indices) res ~= `<line number="%s" hits="%s"/>`.fmt(i, lines[i].times_covered);
@@ -54,15 +54,15 @@ struct CoveredFile {
 
 int main(string[] args) {
 	import std.path: relative_path = relativePath;
-	if (args.length < 2) {
-		writefln("Usage: %s <coverages.lst>");
+	if (args.length < 3) {
+		writefln("Usage: %s <prefix> <coverages.lst>");
 		return 1;
 	}
 
-	foreach (fn; args[1 .. $]) {
+	foreach (fn; args[2 .. $]) {
 		import std.file: fspurt = write;
 		auto c = CoveredFile(fn.relative_path);
-		fspurt(fn[0 .. $-3] ~ "xml", c.xml_dump);
+		fspurt(fn[0 .. $-3] ~ "xml", c.xml_dump(args[1]));
 	}
 
 	return 0;
